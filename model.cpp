323b289c91e26bb462ce31f18bac539d04a76c7c
Lucas Ralph <ralph043@umn.edu>
2022-11-28 13:27:31 -0600

    leaving off of c++

--- model.cpp
+++ model.cpp
@@ -1,9 +1,38 @@
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<array> 
+#include<tuple> 
+#include<string>
+#include<cstring>
+#include<vector>
 #include "stdio.h"
+#include<fstream>
+using namespace std;
 
 class model {
     public:
+
+
+        //constructor with default values
+        model(std::string questionType = "reordering", 
+                int numQuestions = 500, 
+                std::string lmsType = "canvas", 
+                std::string lineTuples = {}, 
+                std::string filePath = "./") {
+            
+            cout << "l" << endl;
+            setQuestionType(questionType);
+            cout<< "g" << endl;
+            setNumQuestions(numQuestions);
+            cout << "h" << endl;
+            setLmsType(lmsType);
+            cout << "i" << endl;
+            setLineTuples(&lineTuples);
+            cout << "j" << endl;
+            setFilePath(filePath);
+            cout << "k" << endl;
+         }
+
+
         std::string getQuestionType(){
             return this->questionType;
         }
@@ -28,12 +57,12 @@ class model {
             this->lmsType = newLmsType;
         }
 
-        std::vector<std::string> getLineTuples(){
-            return this->lineTuples;
+        string* getLineTuples(){
+            return *(this->lineTuples);
         }
 
-        void setLineTuples(std::vector<std::string> newLineTuples){
-            this->lineTuples = newLineTuples;
+        void setLineTuples(string* newLineTuples){
+            *(this->lineTuples) = newLineTuples;
         }
 
         std::string getFilePath(){
@@ -49,7 +78,15 @@ class model {
         std::string questionType = "reorder";
         int numQuestions = 500;
         std::string lmsType = "canvas";
-        std::vector<std::string> lineTuples;
+        string* lineTuples[500];
         std::string filePath = NULL;
         
 };
+
+
+
+//  int main(int argc, char const *argv[])
+//     {
+
+//         return 0;
+//     }

5ae66f432b98f258b6578bc46ffba5c0486cbfaa
Lucas Ralph <ralph043@umn.edu>
2022-12-10 20:59:32 -0600

    Lucas adding a helper function and combining each variation together with the original string together

--- controller.py
+++ controller.py
@@ -1,4 +1,6 @@
+import random
 from model import Model
+from question import Question
 
 #class Controller:
 
@@ -103,20 +105,111 @@ def chooseSettings():
         print(line)
 
     #declare new model object needed for question generator and later formatting for exporting to lms system
-    model = Model(lms_type, question_type, num_questions, lines)
+    global model 
+    model= Model(lms_type, question_type, num_questions, lines)
+
+
+    def MCQuestionGeneratorHelper(correct_line):
+
+        #create a list to hold the correct line and 3 mutants
+
+        options = []
+
+        #loop for 3 iterations, in each iteration create a variation of the correct line by reordering the elements of the line and adding it to the list
+        
+        for i in range(3):
+                
+                #create a list of the elements of the line
+    
+                line_list = correct_line.split()
+    
+                #shuffle the list
+    
+                random.shuffle(line_list)
+    
+                #convert the list back into a string
+    
+                line = " ".join(line_list)
+    
+                #check if the line is already in the list, if it is, re run the loop, otherwise add it to the list
+
+                if line in options:
+                    i = i - 1
+                else:
+                    options.append(line)
+                
+        #add the correct line to the list and shuffle the list
+
+        options.append(correct_line)
+        random.shuffle(options)
+
+        #create a variable to hold the index of the correct line in the list
+
+        correct_index = options.index(correct_line)
+
+        #create a question object with the list of options and the index of the correct line and return it
+
+        q1 = Question(options, correct_index)
+        return q1
+
+                
+
+            
+
+
 
     def generateQuestions(model):
-        #functionality to be added in later versions
 
+        #creating a file to output
+
+        f = open("output.txt", "x")
+
+        #create a list of objects named "questionsToReturn"
+
+        questionsToReturn = []
+
+        reachedEOF = False
         question_type = model.get_question_type()
         questions = model.get_lines()
         numOfQuestions = model.get_num_questions()
 
-        # create a sub list of lines 
-        
-        for each line in questions:
-            # create a sub list of this line, or maybe we're going to want to 
-            # create an object using question as a class so we can make an object out of it
-              
+               #functionality to be added in later versions
+
+        #if question type is "mc" multiple choice code for multiple choice goes here
+
+        if (question_type == "mc"):
+
+            #break down list into sublist
+
+            if (len(questions) < num_questions):
+                exit # have to fix this later, edge case that will need to be handled
+            
+            for line in questions:
+                if(numOfQuestions > 0 or reachedEOF):
+                    questionsToReturn.append(MCQuestionGeneratorHelper(line))
+
+            # create a sub list of this line, or maybe create an object 
+            
+                # generate 3 mutants with one element of the list out of place
+
+            # using question as a class so we can make an object out of it
+
+            #reorder indicies of sublist into 3 variations
+
+            #combine 3 variations of sublist with original list
+
+
+
+        #if question type is fill in the blank code for fill in the blank goes here
+
+            #break down each list into sublist
+
+            #delete indecies to create variations of sublist
+
+            #recombine variations of sublist with original sublist
+
+
+        f.close()
+
 
         pass
